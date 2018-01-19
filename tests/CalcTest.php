0deed621ad728b422206905e359c2d879f15c8ac
moe10n77 <moe10n77@gmail.com>
2018-01-19 11:08:24 +0900

    add

--- tests/CalcTest.php
+++ tests/CalcTest.php
@@ -8,7 +8,7 @@ class CalcTest extends TestCase
     public function testAdd()
     {
 	$calc = new Calc();
-	$this->assertEquals($calc->add(3, 2), 1);
+	$this->assertEquals($calc->add(1, 2), 3);
     }
 }
 ?>
